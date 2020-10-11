b2940bde5427f61791a99af886d199401fbdbcea
Välimaa Sini EXT <ext-sini.valimaa@elisa.fi>
2020-10-11 16:13:10 +0300

    muutoksia pom

--- src/main/java/fi/pajunen/BabyDiary/web/EntryController.java
+++ src/main/java/fi/pajunen/BabyDiary/web/EntryController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import fi.pajunen.BabyDiary.domain.CategoryRepository;
 import fi.pajunen.BabyDiary.domain.Entry;
 import fi.pajunen.BabyDiary.domain.EntryRepository;
-import s2020.Bookstore.domain.Book;
 
 @Controller
 public class EntryController {
